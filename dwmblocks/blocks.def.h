ccd1589310ae4cb2ba4818501df7322052f2c3c3
setephano <setephano@gmail.com>
2022-03-15 03:50:17 +1300

    adding projects

---
+++ dwmblocks/blocks.def.h
@@ -0,0 +1,17 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"Memory: ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"", "acpi",					300,		0},
+
+	{"IP(L): ", "ifconfig | grep inet | awk '{print $2}' | grep 192",					5,		0},
+
+	//{"IP(E): ", "curl ifconfig.me",					5,		0},
+
+	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
