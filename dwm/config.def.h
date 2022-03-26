65faabbfd4111cecb771032e262362eee0382938
setephano <setephano@gmail.com>
2022-03-26 14:59:29 +1300

    Adding further configuration to environment

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int gappx     = 5;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -13,12 +13,16 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
+static const char col_ecstasy[]		= "#fb7e14";
+static const char col_black[]		= "#000000";
+static const char col_codgray[]		= "#151515";
+static const char col_mineshaft[]	= "#292929";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_ecstasy,  col_ecstasy  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
