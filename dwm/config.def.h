f13db6d7a87f2bbfefda1b7008f1cd4cf40d7edd
Setephano <setephano@gmail.com>
2022-07-25 22:44:29 +1200

    Reset & Revert of previous configuration file for dwm

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -45,7 +45,7 @@ static const Rule rules[] = {
 	/* class      	     instance    title    tags mask     isfloating   CenterThisWindow?     monitor */
 	{ "st",              NULL,       NULL,    0,            0,     	     1,					   -1 },
 	{ "Gimp",            NULL,       NULL,    0,            1,           0,                    -1 },
-	{ "Firefox",         NULL,       NULL,    0,       0,           0,                    -1 }, //Previous mask 1 << 8
+	{ "Firefox",         NULL,       NULL,    1 << 8,       0,           0,                    -1 }, //Previous mask 1 << 8
 };
 
 /* layout(s) */
