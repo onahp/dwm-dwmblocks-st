a6d8488eed506f915ea70fcaeb8c5228ae81bbc0
Setephano <setephano@gmail.com>
2022-07-25 14:57:31 +1200

    Testing tag rules before running make clean install

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -43,9 +43,11 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      	     instance    title    tags mask     isfloating   CenterThisWindow?     monitor */
-	{ "st",              NULL,       NULL,    0,            0,     	     1,		           -1 },
+	{ "st",              NULL,       NULL,    0,            0,     	     1,					   -1 },
 	{ "Gimp",            NULL,       NULL,    0,            1,           0,                    -1 },
-	{ "Firefox",         NULL,       NULL,    1 << 8,       0,           0,                    -1 },
+	{ "firefox",         NULL,       NULL,    1 << 8,       0,           0,                    -1 },
+	{ "1password",       NULL,       NULL,    1 << 8,       0,           0,                    -1 },
+	{ "keepassxc",       NULL,       NULL,    1 << 8,       0,           0,                    -1 },
 };
 
 /* layout(s) */
