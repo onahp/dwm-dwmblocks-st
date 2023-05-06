5fa4c8e2477721db9b2a4bb3e144935f5ca5ba20
Setephano <setephano@gmail.com>
2023-05-06 20:16:04 +1200

    integration: changes to terminal flexibility with alacritty default

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -6,7 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -43,7 +43,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      	     instance    title    tags mask     isfloating   CenterThisWindow?     monitor */
-	{ "st",              NULL,       NULL,    0,            0,     	     1,					   -1 },
+	{ "st",              NULL,       NULL,    0,            0,     	     1,					           -1 },
 	{ "Gimp",            NULL,       NULL,    0,            1,           0,                    -1 },
 	{ "Firefox",         NULL,       NULL,    1 << 8,       0,           0,                    -1 }, //Previous mask 1 << 8
 };
@@ -100,16 +100,22 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+/* static const char *termcmd[]  = { "st", NULL }; */
 static const char scratchpadname[] = "scratchpad";
+static const char alacrittyname[] = "alacritty";
 static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
+static const char *alacritty[] = { "alacritty", "-T", alacrittyname, NULL };
+static const char *firefox[] = { "firefox", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } },
-	{ MODKEY,                       XK_w,      spawn,          SHCMD("firefox")},
+	{ MODKEY,                       XK_grave,  togglescratch,  {.v = alacritty } },
+	/* { MODKEY,                       XK_grave,  togglescratch,  {.v = scratchpadcmd } }, */
+	{ MODKEY,                       XK_w,      spawn,          {.v = firefox }},
+	/* { MODKEY,                       XK_w,      spawn,          SHCMD("firefox")}, */
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	STACKKEYS(MODKEY,                          focus)
 	STACKKEYS(MODKEY|ShiftMask,                push)
