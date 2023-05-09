df21a8491c11a54c8bc6eab1ec7c78dde1cb3a46
Setephano <setephano@gmail.com>
2023-05-10 09:55:16 +1200

    dwm: (main) restructured base layout

--- dwm/backup.config.h
+++ dwm/backup.config.h
@@ -10,17 +10,21 @@ static const unsigned int gappov    = 30;       /* vert outer gap between window
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
+static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
+/* static const char col_gray1[]       = "#0D1117"; */
+/* static const char col_gray2[]       = "#0D1117"; */
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
-static const char col_ecstasy[]		= "#fb7e14";
+static const char col_ecstasy[]		= "#FF4800";
 static const char col_black[]		= "#000000";
-static const char col_codgray[]		= "#151515";
-static const char col_mineshaft[]	= "#292929";
+static const char col_codgray[]		= "#0D1117";
+static const char col_mineshaft[]	= "#0D1117";
+/* static const char col_codgray[]		= "#151515"; */
+/* static const char col_mineshaft[]	= "#292929"; */
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
--- dwm/config.def.h
+++ dwm/config.def.h
@@ -1,25 +1,34 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */ static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 32;       /* snap pixel */ 
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=12" };
+static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
+/* static const char col_gray1[]       = "#222222"; */
+/* static const char col_gray2[]       = "#444444"; */
+static const char col_gray1[]       = "#0D1117";
+static const char col_gray2[]       = "#0D1117";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char col_ecstasy[]		= "#fb7e14";
+static const char col_cyan[]        = "#AA280B";
+/* static const char col_cyan[]        = "#005577"; */
+/* static const char col_ecstasy[]		= "#fb7e14"; */
+/* static const char col_ecstasy[]		= "#FF4800"; */
+/* static const char col_ecstasy[]		= "#C2391A"; */
+static const char col_ecstasy[]		= "#AA280B";
 static const char col_black[]		= "#000000";
-static const char col_codgray[]		= "#151515";
-static const char col_mineshaft[]	= "#292929";
+static const char col_codgray[]		= "#0D1117";
+static const char col_mineshaft[]	= "#0D1117";
+/* static const char col_codgray[]		= "#151515"; */
+/* static const char col_mineshaft[]	= "#292929"; */
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
