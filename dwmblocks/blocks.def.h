f7a4057e1932cbbc12a76e33efa46076a722722b
Setephano <setephano@gmail.com>
2023-08-02 22:54:43 +1200

    mod(dwmblocks): changes to batter life usage if any

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -3,6 +3,8 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"Memory: " , "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	10,		0},
 
+	{"" , "acpi",	300,		0},
+
 	{"CPU: "     , "sensors | awk '/Core 0/ {print $3}'", 30,		0},
 
 	{"IP(L): " , "ip address show enp4s0 | grep inet | sed -n '1p' | awk '{print $2}'",					5,		0},
