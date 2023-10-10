3b60d8324f84fd9c16336d3381e7958d322942cc
Setephano <setephano@gmail.com>
2023-10-10 20:53:59 +1300

    add(dwm): changes to remove alt x

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -103,7 +103,7 @@ static const Layout layouts[] = {
 	{ MOD, XK_q,     ACTION##stack, {.i = 0 } }, \
 	{ MOD, XK_a,     ACTION##stack, {.i = 1 } }, \
 	{ MOD, XK_z,     ACTION##stack, {.i = 2 } }, \
-	{ MOD, XK_x,     ACTION##stack, {.i = -1 } },
+	/* { MOD, XK_x,     ACTION##stack, {.i = -1 } }, */
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/usr/local/bin/st", "-c", cmd, NULL } }
